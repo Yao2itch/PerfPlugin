b70282ba06177c5e58934b2d6a644c3b615d9859
Yao2itch <1032145192@qq.com>
2020-04-18 17:29:11 +0800

    #Yao 添加脚本

--- Code/PerfPlugin/PerfPlugin/Src/PerfPlugin.cs
+++ Code/PerfPlugin/PerfPlugin/Src/PerfPlugin.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace PerfPlugin
 {
     public class PerfPlugin
     {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MEMORY_INFO
+        {
+            public uint  dwLength; //当前结构体大小
+            public uint  dwMemoryLoad; //当前内存使用率
+            public ulong ullTotalPhys; //总计物理内存大小
+            public ulong ullAvailPhys; //可用物理内存大小
+            public ulong ullTotalPageFile; //总计交换文件大小
+            public ulong ullAvailPageFile; //总计交换文件大小
+            public ulong ullTotalVirtual; //总计虚拟内存大小
+            public ulong ullAvailVirtual; //可用虚拟内存大小
+            public ulong ullAvailExtendedVirtual; //保留 这个值始终为0
+        }
+
 
     }
 }
